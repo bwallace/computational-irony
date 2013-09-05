33cf1a948305279beca3a0e694a9732bf0def874
byron wallace <byron.wallace@gmail.com>
2013-09-05 16:06:17 -0400

    moderate progress

--- ironic_alien.py
+++ ironic_alien.py
@@ -4,6 +4,8 @@ user_agent = "ironist v0 by /u/byron https://github.com/bwallace/computational-i
 import BeautifulSoup as bs
 import urllib2
 
+import html2text 
+
 def setup():
     global r
     r = praw.Reddit(user_agent=user_agent)
@@ -17,6 +19,17 @@ def comments_for_thread(thread):
     for comment in comments:
         pass
 
+def _clean_up(some_html):
+    _none_to_empty = lambda s : s if s is not None else ""
+    some_html = _none_to_empty(some_html)
+    return html2text.html2text(some_html)
+
+
+def _remove_js(soup):
+    for script in soup("script"):
+        script.extract()
+    return soup
+
 def get_page(url, thread_id, out_dir="content"):
     page = urllib2.urlopen(url).read()
     # first dump just the page (HTML)
@@ -25,11 +38,18 @@ def get_page(url, thread_id, out_dir="content"):
         f_out.write(page.text_context())
 
     # now parse and dump plaintext (title + body)
-
-
     # we'll use beautiful soup to parse this
     soup = bs(page.text_context())
+    # remove js
+    title, body = "", ""
+    if soup is not None:
+        soup = _remove_js(soup)
+        # find the title and body; ignore everything else
+        title = _clean_up(soup.head.title.text)
+        body = _clean_up(soup.head.body.text)
 
-    # find the title and body; ignore everything else
+    with open(os.path.join(out_dir, thread_id + ".txt")) as f_out:
+        f_out.write("{0}\n\n{1}".format(title, body))
+
 
 
