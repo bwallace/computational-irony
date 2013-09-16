e08d05a6ff926d4480ab0f443430f35feb6804e1
byron wallace <byron.wallace@gmail.com>
2013-09-16 10:02:29 -0400

    progress on app

--- annotation/annotatr/annotatr/urls.py
+++ annotation/annotatr/annotatr/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from irony import views
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    # ex: /irony/5/
+    url(r'^(?P<comment_id>\d+)/$', views.show_comment, name='show_comment'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'})
 )
