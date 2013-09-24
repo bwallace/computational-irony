ad8c8810240f290377947c555ffa7751fe3a6b65
byron wallace <byron.wallace@gmail.com>
2013-09-24 10:09:27 -0400

    more UI functionality

--- annotation/annotatr/irony/views.py
+++ annotation/annotatr/irony/views.py
@@ -6,19 +6,47 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.views.generic import View
 from django.views.generic.base import TemplateView
+from django.utils.safestring import mark_safe
+from django.views.decorators.csrf import csrf_exempt
 
 from irony.models import Comment, CommentSegment, Label
 
 def index(request):
     return HttpResponse("Hello, ironic world.")
 
+def _lines_to_breaks(segments):
+    for segment in segments:
+        segment.text = segment.text.replace("\n", "<br/>")
+    return segments
+
 #@permission_required()
 def show_comment(request, comment_id):
     comment_ = Comment.objects.get(pk=comment_id)
-    
-    segments = CommentSegment.objects.filter(comment=comment_).order_by('segment_index')
-    return render_to_response("annotate.html", {"comment":comment_, "segments":segments})
+    #segments = CommentSegment.objects.filter(comment=comment_).order_by('segment_index')
+    #return render_to_response("annotate.html", {"comment":comment_, "segments":segments})
+    return render_to_response("annotate.html", {"comment":comment_})
 
+#def next_comment_segments()
+def get_comment_segments(request, comment_id):
+    comment_ = Comment.objects.get(pk=comment_id)
+    segments = CommentSegment.objects.filter(comment=comment_).order_by('segment_index')
+    return render_to_response("comment_fragment.html", {"segments":segments})
+
+def annotate_segments(request):
+    ironic_segment_ids = request.POST.getlist('ironic_segments[]')
+    # we are storing these as s{segment_id}; so just remove the 
+    # prepended s.
+    ironic_segment_ids = [int(s_id.split("s")[1]) for s_id in ironic_segment_ids]
+    # get and label these segments
+
+
+    ### TMP TMP TMP
+    comment_ = Comment.objects.get(pk=1)
+    segments = CommentSegment.objects.filter(comment=comment_).order_by('segment_index')
+    ## END TMP
+
+    return render_to_response("comment_fragment.html", 
+                                {"comment":comment_, "segments":segments})
 
 def login(request):
     pass
